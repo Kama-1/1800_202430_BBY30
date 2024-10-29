0ad314eefd81de835b5bc89c66d838b6969ed462
Anna Jang <aj3spencer@gmail.com>
2024-10-29 14:43:01 -0700

    Finished first version of assignments page

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,5 +2,6 @@
 function loadSkeleton() {
     console.log($("#navbar").load('./skeleton/navbar.html'));
     console.log($(".assignment").load('./skeleton/assignment.html'));
+    console.log($("#dropdown").load('./skeleton/dropdown.html'));
 }
 loadSkeleton();
