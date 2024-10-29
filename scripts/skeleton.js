c18d0ed00eb7ee9754767599b19a6f016c6f83bd
Anna Jang <aj3spencer@gmail.com>
2024-10-29 13:59:06 -0700

    Responsive example assignment

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,6 +1,6 @@
 //Loads the navbar
 function loadSkeleton() {
     console.log($("#navbar").load('./skeleton/navbar.html'));
-    console.log($("#assignment").load('./skeleton/assignment.html'));
+    console.log($(".assignment").load('./skeleton/assignment.html'));
 }
 loadSkeleton();
