5c51a4f5884b3ec486061e387f009efbb36cbe27
Jonah Botelho <jbotelho3@my.bcit.ca>
2024-10-31 21:48:15 -0700

    Stored user settings in firestore (except profile pic)

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,36 +1,43 @@
-// Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      var user = authResult.user;
+      if (authResult.additionalUserInfo.isNewUser) {
+        db.collection("users").doc(user.uid).set({
+          //User information to be saved
+          name: user.displayName,
+          course_list_startup: false,
+          website_theme: light,
+          points: 0,
+        }).then(function () {
+          window.location.assign("assignments.html");
+        }).catch(function (error) {
+          console.log("Error adding new user: " + error);
+        });
+      } else {
         return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
       }
+      return false;
     },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: "assignments.html",
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
+    uiShown: function () {
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  signInFlow: 'popup',
+  signInSuccessUrl: "assignments.html",
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
     //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
-  };
+  ],
 
-  ui.start('#firebaseui-auth-container', uiConfig);
+  // tosUrl: '<your-tos-url>',
+  // privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+ui.start('#firebaseui-auth-container', uiConfig);
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,10 +1,34 @@
-function getNameFromAuth() {
+function getName() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             userName = user.displayName;
             document.getElementById("changeName").value = userName;
         }
-            
+    });
+}
+
+function getWebsiteTheme() {
+    firebase.auth().onAuthStateChanged(user => {
+        db.collection("users").doc(user.uid).get().then((doc) => {
+            if (doc.exists) {
+                const websiteTheme = doc.data().website_theme;
+                document.getElementById("websiteTheme").value = websiteTheme; 
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
+    });
+}
+
+function getShowCoursesOnStartup() {
+    firebase.auth().onAuthStateChanged(user => {
+        db.collection("users").doc(user.uid).get().then((doc) => {
+            if (doc.exists) {
+                document.getElementById("showCoursesOnStartup").checked = doc.data().course_list_startup;
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
     });
 }
 
@@ -12,10 +36,11 @@ function updateName() {
     firebase.auth().onAuthStateChanged(user => {
         let username = document.getElementById("changeName").value;
         if (user) {
+            db.collection("users").doc(user.uid).set({
+                name: user.displayName,
+            }, { merge: true }).then
             user.updateProfile({
-                displayName: username 
-            }).then(() => {
-                console.log("Username updated successfully");
+                displayName: username
             }).catch((error) => {
                 console.error("Error updating username:", error);
             });
@@ -45,4 +70,27 @@ function updatePassword() {
     });
 }
 
-getNameFromAuth();
+function updateShowCoursesOnStartup() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            const checkbox = document.getElementById("showCoursesOnStartup");
+            db.collection("users").doc(user.uid).set({
+                course_list_startup: checkbox.checked,
+            }, { merge: true })
+        }
+    });
+}
+
+function updateWebsiteTheme() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            const theme = document.getElementById("websiteTheme").value;
+            db.collection("users").doc(user.uid).set({
+                website_theme: theme,
+            }, { merge: true })
+        }
+    });
+}
+getName();
+getWebsiteTheme();
+getShowCoursesOnStartup();
