4e5a636a696dfd6834978f51d7dbfc048cb62adc
Jonah Botelho <jbotelho3@my.bcit.ca>
2024-11-03 15:13:40 -0800

    Fixed addAssignment

--- scripts/addAssignment.js
+++ scripts/addAssignment.js
@@ -2,16 +2,19 @@ function addAssignment() {
     const newTitle = document.getElementById("editTitle").value;
     const newTag = document.getElementById("editCourseTag").value;
     const newDescription = document.getElementById("editDescription").value;
-    const tempDueDate1 = document.getElementById("editDueDate").value;
-    const tempDueDate2 = new Date(tempDueDate1);
-    const newDueDate = firebase.firestore.Timestamp.fromDate(tempDueDate2);
+    const newDueDate = firebase.firestore.Timestamp.fromDate(new Date(document.getElementById("editDueDate").value));
     
     db.collection("Assignments").doc(newTitle).set({
         title: newTitle,
         course_tag: newTag,
         description: newDescription,
-        dueDate: newDueDate,
+        due_date: newDueDate,
         users_completed: 0,
     }, { merge: true })
+    .then(() => {
+        setTimeout(() => {
+            location.href = 'assignments.html';
+        }, 250); 
+    })
 }
 
