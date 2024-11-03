659a1b52ee8f3fc5fea7acfcb49cf1d1885fda1c
Jonah Botelho <jbotelho3@my.bcit.ca>
2024-11-03 14:26:35 -0800

    Added due date picker (Unfinished)

--- scripts/addAssignment.js
+++ scripts/addAssignment.js
@@ -2,12 +2,15 @@ function addAssignment() {
     const newTitle = document.getElementById("editTitle").value;
     const newTag = document.getElementById("editCourseTag").value;
     const newDescription = document.getElementById("editDescription").value;
-
+    const tempDueDate1 = document.getElementById("editDueDate").value;
+    const tempDueDate2 = new Date(tempDueDate1);
+    const newDueDate = firebase.firestore.Timestamp.fromDate(tempDueDate2);
+    
     db.collection("Assignments").doc(newTitle).set({
         title: newTitle,
         course_tag: newTag,
         description: newDescription,
-        dueDate: null,
+        dueDate: newDueDate,
         users_completed: 0,
     }, { merge: true })
 }
