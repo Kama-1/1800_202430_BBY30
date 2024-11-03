b0372d176fb60c57f715abc83478613130ace6ef
Kevin Ung <ungkevin16@gmail.com>
2024-11-03 00:43:46 -0700

    added leaderboard that ranks based on total points

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -1,6 +1,18 @@
 function leaderboard() {
   //Replace leaderboard template
-  let leaderboardTable = document.getElementById("leaderboard").getElementsByTagName("tbody")[0];
+  let tableTemplate = document.getElementById("leaderboard-goes-here").getElementsByTagName('tbody')[0];
+  let rank = 1;
 
+  db.collection("users").orderBy("points", "desc").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        const row = tableTemplate.insertRow();
+        row.insertCell(0).textContent = rank++;
+        row.insertCell(1).textContent = data.name;
+        row.insertCell(2).textContent = data.points;
+    });
+    }).catch((error) => {
+      console.error("Error fetching leaderboard data: ", error); 
+    });
 }
 leaderboard();
