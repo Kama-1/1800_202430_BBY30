3ea7a88cc89b0188428bd91189ddf63dc3d23d12
Jonah Botelho <jbotelho3@my.bcit.ca>
2024-11-15 15:10:10 -0800

    Completed deleting assignments

--- scripts/admin.js
+++ scripts/admin.js
@@ -7,22 +7,25 @@ function deleteAssignment(assignment_id) {
     .then(() => {
       setTimeout(() => {
         location.reload();
-      }, 500);
+      }, 1000);
     });
 
   // Removes assignment from users array
-//   db.collection("users")
-//     .get()
-//     .then((user) => {
-//       user.forEach((doc) => {
-//         let assignmentsArray = doc.data().completedAssignments;
-//         assignmentsArray.splice(assignment_id);
-//         db.collection("users").doc(doc.id).set(
-//           {
-//             completedAssignments: assignmentsArray,
-//           },
-//           { merge: true }
-//         );
-//       });
-//     });
+  db.collection("users")
+    .get()
+    .then((user) => {
+      user.forEach((doc) => {
+        console.log("what");
+        let assignmentsArray = doc.data().completedAssignments;
+        let assignmentIndex = assignmentsArray.map(i => i.assignment_id).indexOf(assignment_id);
+        console.log(assignmentIndex);
+        assignmentsArray.splice(assignmentIndex, 1);
+        db.collection("users").doc(doc.id).set(
+          {
+            completedAssignments: assignmentsArray,
+          },
+          { merge: true }
+        );
+      });
+    });
 }
